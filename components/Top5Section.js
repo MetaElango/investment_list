a78c55a46f4c7bd8b69432a11ed8e39a6bc359e4
Elangodevguy <elangodevguy@gmail.com>
2023-02-28 10:28:22 +0530

    moved data to different file

--- components/Top5Section.js
+++ components/Top5Section.js
@@ -10,62 +10,7 @@ import {
   Link,
   OrderedList,
 } from "@chakra-ui/react";
-const top5Data = [
-  {
-    name: "Hedonova",
-    rating: "4.8",
-    link: "https://www.hedonova.io/",
-    linkPara: `A visionary hedge fund that brings a new era of investing to the
-    forefront. Founded in 2020 by Alexander Cavendish, CEO, and Suman
-    Bannerjee, CIO, Hedonova comprises a team of 16 seasoned
-    professionals including investors, lawyers, economists,
-    researchers, engineers, and marketers.`,
-    paras: [
-      `Hedonova's securities are SEC qualified, ensuring the highest
-    level of security and protection for investor funds. With a
-    globally diversified portfolio offering access to over 10
-    alternative assets, including works of art, high-yield real
-    estate, cutting-edge startups, cryptos, NFTs, Web3, the
-    agricultural economy, top university students, equipment
-    financing, media royalties, wine and more, Hedonova provides
-    investors with a new level of freedom and access to investment
-    opportunities not limited by political borders and national
-    policies.`,
-      `In the past, such investment options were only available to
-    high-net-worth individuals. However, Hedonova is open to everyone,
-    with smaller entry points to allow anyone who meets minimum
-    requirements access to these alternative investment opportunities.
-    Registered as a Delaware LLC 506(b) exempted company, Hedonova
-    offers a 100% tax pass-through, with investors paying taxes
-    according to local laws. The fund also provides professional
-    support for investors with regard to taxation, compliance, and
-    other related services.`,
-    ],
-    pros: [
-      `Barrier-free entry into alternative investments: Hedonova offers a simple and straightforward way for individuals to make alternative investments, regardless of prior expertise.`,
-
-      `Low investment threshold: The low minimum investment requirement of $5,000 makes Hedonova accessible to a wide range of investors, both inside and outside of the U.S.`,
-
-      `Flexible investment options:  With no hold period, investors have the freedom to enter and exit the fund as they please, giving them greater control over their investments.`,
-
-      `Diversified portfolio: Hedonova provides access to a range of alternative assets through a single, globally diversified fund, giving investors the opportunity to diversify their investment portfolio and potentially increase returns.`,
-
-      `IRA and 401(k) investment options: Hedonova offers the ability for individuals to invest via their IRA or 401(k), allowing them to take advantage of the benefits of these investment vehicles while also accessing alternative assets.`,
-
-      `Open to all levels of investors: Hedonova is open to all investor levels, providing a level playing field for everyone to access alternative investment opportunities.`,
-    ],
-    cons: [
-      `Limited investment options: As a mutual fund, Hedonova only offers a single investment option, limiting the flexibility and choice available to investors.`,
-
-      `Lack of a proven track record: Being a relatively new company, Hedonova has a limited track record and it is too early to determine its long-term potential, making it a riskier investment choice for some investors.`,
-
-      `No individual control over asset allocation: Investors do not have the option to select the alternative asset allocation themselves, with the fund manager making all asset allocation decisions based on global market conditions and risk factors. This lack of control over investment strategy may be a disadvantage for some investors.`,
-    ],
-    minInv: `$ 5,000`,
-    asstClass: `Multi Asset class (10+ alternative assets)`,
-  },
-];
-
+import { top5Data } from "./top5data";
 export default function Top5Section() {
   return (
     <Container maxW={"7xl"} backgroundColor={"#1D1D1D"}>
@@ -82,36 +27,14 @@ export default function Top5Section() {
           Top 5
         </Heading> */}
         <OrderedList fontSize={"1.2rem"} color={"white"} marginTop={"1rem"}>
-          <ListItem>
-            <Link textDecoration={"underline"} href="#first">
-              Hedonova
-            </Link>
-            <Text as={"span"}>- 4.8</Text>
-          </ListItem>
-          <ListItem>
-            <Link textDecoration={"underline"} href="#second">
-              Cadre
-            </Link>
-            <Text as={"span"}>- 4.8</Text>
-          </ListItem>
-          <ListItem>
-            <Link textDecoration={"underline"} href="#third">
-              Farm together
-            </Link>
-            <Text as={"span"}>- 4.8</Text>
-          </ListItem>
-          <ListItem>
-            <Link textDecoration={"underline"} href="#fourth">
-              Royalty exchange
-            </Link>
-            <Text as={"span"}>- 4.8</Text>
-          </ListItem>
-          <ListItem>
-            <Link textDecoration={"underline"} href="#fifth">
-              Forge
-            </Link>
-            <Text as={"span"}>- 4.8</Text>
-          </ListItem>
+          {top5Data.map((el, index) => (
+            <ListItem key={`top5-0${index}`}>
+              <Link textDecoration={"underline"} href={`#${el.id}`}>
+                {el.name}
+              </Link>
+              <Text as={"span"}>- {el.rating}</Text>
+            </ListItem>
+          ))}
         </OrderedList>
       </Box>
       <Box
@@ -124,27 +47,31 @@ export default function Top5Section() {
       >
         {top5Data.map((el) => (
           <Box>
-            <Heading color="white" as={"h3"} id="first">
+            <Heading color="white" as={"h3"} id={el.id}>
               {el.name}- {el.rating}
             </Heading>
-            <Text color={"#D1ED82"} fontWeight={"bold"} marginTop={"1rem"}>
+            <Text color={"#ffffffb3"} fontWeight={"bold"} marginTop={"1rem"}>
               What is {el.name}?
             </Text>
             <Text color={"white"} marginTop={"1rem"}>
-              <Link href={el.link} isExternal>
+              <Link color={"#D1ED82"} href={el.link} isExternal>
                 {el.name} <ExternalLinkIcon />{" "}
               </Link>
               - {el.linkPara}
             </Text>
-            {el.paras.map((para) => (
-              <Text color={"white"} marginTop={"1rem"}>
+            {el.paras.map((para, index) => (
+              <Text
+                key={`${el.name}-para-0${index}`}
+                color={"white"}
+                marginTop={"1rem"}
+              >
                 {para}
               </Text>
             ))}
             <Box marginTop={"1rem"}>
               <Text
                 as={"span"}
-                color={"#D1ED82"}
+                color={"#ffffffb3"}
                 marginTop={"1rem"}
                 fontWeight={"bold"}
               >
@@ -157,7 +84,7 @@ export default function Top5Section() {
             <Box marginTop={"1rem"}>
               <Text
                 as={"span"}
-                color={"#D1ED82"}
+                color={"#ffffffb3"}
                 marginTop={"1rem"}
                 fontWeight={"bold"}
               >
@@ -168,24 +95,32 @@ export default function Top5Section() {
               </Text>
             </Box>
             <Box marginTop={"1rem"}>
-              <Text color={"#D1ED82"} fontWeight={"bold"}>
+              <Text color={"#ffffffb3"} fontWeight={"bold"}>
                 Pros:
               </Text>
               <UnorderedList paddingLeft={"2rem"}>
-                {el.pros.map((pro) => (
-                  <ListItem color={"white"} marginTop={"1rem"}>
+                {el.pros.map((pro, index) => (
+                  <ListItem
+                    key={`${el.name}-pro-0${index}`}
+                    color={"white"}
+                    marginTop={"1rem"}
+                  >
                     {pro}
                   </ListItem>
                 ))}
               </UnorderedList>
             </Box>
             <Box marginTop={"1rem"} marginBottom={"5rem"}>
-              <Text color={"#D1ED82"} fontWeight={"bold"}>
+              <Text color={"#ffffffb3"} fontWeight={"bold"}>
                 Cons:
               </Text>
               <UnorderedList paddingLeft={"2rem"}>
-                {el.cons.map((con) => (
-                  <ListItem color={"white"} marginTop={"1rem"}>
+                {el.cons.map((con, index) => (
+                  <ListItem
+                    key={`${el.name}-con-0${index}`}
+                    color={"white"}
+                    marginTop={"1rem"}
+                  >
                     {con}
                   </ListItem>
                 ))}
---
+++ components/top5data.js
@@ -0,0 +1,166 @@
+export const top5Data = [
+  {
+    id: "first",
+    name: "Hedonova",
+    rating: "4.8",
+    link: "https://www.hedonova.io/",
+    linkPara: `A visionary hedge fund that brings a new era of investing to the
+      forefront. Founded in 2020 by Alexander Cavendish, CEO, and Suman
+      Bannerjee, CIO, Hedonova comprises a team of 16 seasoned
+      professionals including investors, lawyers, economists,
+      researchers, engineers, and marketers.`,
+    paras: [
+      `Hedonova's securities are SEC qualified, ensuring the highest
+      level of security and protection for investor funds. With a
+      globally diversified portfolio offering access to over 10
+      alternative assets, including works of art, high-yield real
+      estate, cutting-edge startups, cryptos, NFTs, Web3, the
+      agricultural economy, top university students, equipment
+      financing, media royalties, wine and more, Hedonova provides
+      investors with a new level of freedom and access to investment
+      opportunities not limited by political borders and national
+      policies.`,
+      `In the past, such investment options were only available to
+      high-net-worth individuals. However, Hedonova is open to everyone,
+      with smaller entry points to allow anyone who meets minimum
+      requirements access to these alternative investment opportunities.
+      Registered as a Delaware LLC 506(b) exempted company, Hedonova
+      offers a 100% tax pass-through, with investors paying taxes
+      according to local laws. The fund also provides professional
+      support for investors with regard to taxation, compliance, and
+      other related services.`,
+    ],
+    pros: [
+      `Barrier-free entry into alternative investments: Hedonova offers a simple and straightforward way for individuals to make alternative investments, regardless of prior expertise.`,
+
+      `Low investment threshold: The low minimum investment requirement of $5,000 makes Hedonova accessible to a wide range of investors, both inside and outside of the U.S.`,
+
+      `Flexible investment options:  With no hold period, investors have the freedom to enter and exit the fund as they please, giving them greater control over their investments.`,
+
+      `Diversified portfolio: Hedonova provides access to a range of alternative assets through a single, globally diversified fund, giving investors the opportunity to diversify their investment portfolio and potentially increase returns.`,
+
+      `IRA and 401(k) investment options: Hedonova offers the ability for individuals to invest via their IRA or 401(k), allowing them to take advantage of the benefits of these investment vehicles while also accessing alternative assets.`,
+
+      `Open to all levels of investors: Hedonova is open to all investor levels, providing a level playing field for everyone to access alternative investment opportunities.`,
+    ],
+    cons: [
+      `Limited investment options: As a mutual fund, Hedonova only offers a single investment option, limiting the flexibility and choice available to investors.`,
+
+      `Lack of a proven track record: Being a relatively new company, Hedonova has a limited track record and it is too early to determine its long-term potential, making it a riskier investment choice for some investors.`,
+
+      `No individual control over asset allocation: Investors do not have the option to select the alternative asset allocation themselves, with the fund manager making all asset allocation decisions based on global market conditions and risk factors. This lack of control over investment strategy may be a disadvantage for some investors.`,
+    ],
+    minInv: `$5,000`,
+    asstClass: `Multi Asset class (10+ alternative assets)`,
+  },
+  {
+    id: "second",
+    name: "Cadre",
+    rating: "3.5",
+    link: "https://cadre.com/",
+    linkPara: `is a financial technology company based in New York that provides a unique platform for individuals and institutions to directly invest in large commercial properties. Unlike traditional real estate investing, Cadre's platform allows investors to select specific transactions to participate in and invest a smaller amount than what is typically required for a full transaction. Additionally, Cadre offers a managed portfolio service, a real estate secondary market, and a cash holdings account called "Cadre Cash". The company has received funding from various venture capital sources, including Thrive Capital and General Catalyst, which invested $18 million during a 2015 fundraising round. Cadre was also recognized as one of Forbes' "FinTech50" companies for four consecutive years starting in 2016.`,
+    paras: [
+      `Ryan Williams, Cadre's founder and CEO, believes that the company's ability to disrupt the real estate industry is due to the fact that it is an outdated industry that has not fully embraced technology. Cadre's investment strategy focuses on quality over quantity, targeting the best properties in high-growth markets to provide accredited investors with direct access to institutional-quality assets. With a minimum investment of $25,000, Cadre has a strong historical performance, although it is important to note that the platform is only available to accredited investors and does not guarantee liquidity. Through a combination of technology, data, and extensive manual due diligence, Cadre identifies the best investment opportunities in multi-family apartment buildings, offices, hotels, and industrial properties.`,
+    ],
+    pros: [
+      `Strong historical performance: Cadre has a track record of strong performance in the real estate industry, offering investors the opportunity to invest in high-quality commercial properties in high-growth markets. This has resulted in attractive returns for investors in the past.`,
+
+      `Very selective vetting process: Cadre uses a thorough vetting process to identify and evaluate the best investment opportunities. This process includes extensive manual due diligence, data analysis, and technology, ensuring that investors are presented with high-quality investment options.`,
+
+      `Opportunity for liquidity through the Cadre Secondary Market: Cadre offers a secondary market for its investors, providing an opportunity to sell their investment before the end of the investment term. This feature adds an extra level of liquidity and flexibility to the investment process.`,
+    ],
+    cons: [
+      `Minimum $25,000 investment: The minimum investment amount for Cadre is $25,000, which can be a high barrier to entry for some investors.`,
+
+      `For accredited investors only: Cadre is only available to accredited investors, meaning that investors need to meet certain income or net worth requirements to invest. This may exclude a large pool of potential investors who do not meet these requirements.`,
+
+      `Doesn’t guarantee liquidity: While Cadre offers a secondary market, it does not guarantee liquidity. The liquidity of an investment is subject to market conditions, and there is no guarantee that investors will be able to sell their investment at the desired time or price.`,
+    ],
+    minInv: `$25,000`,
+    asstClass: `Real Estate`,
+  },
+  {
+    id: "third",
+    name: "FarmTogether",
+    rating: "4.0",
+    link: "https://farmtogether.com/",
+    linkPara: `is a farmland investment manager that enables accredited investors to invest in US farmland. The investment in farmland has been historically popular among ultra-wealthy investors, as it has a slow, steady, and stable return and is historically uncorrelated to any other index. However, investing in farmland has been challenging due to factors such as knowledge of the market, location in rural areas, and high prices.`,
+    paras: [
+      `Strong historical performance: Cadre has a track record of strong performance in the real estate industry, offering investors the opportunity to invest in high-quality commercial properties in high-growth markets. This has resulted in attractive returns for investors in the past.`,
+      `Very selective vetting process: Cadre uses a thorough vetting process to identify and evaluate the best investment opportunities. This process includes extensive manual due diligence, data analysis, and technology, ensuring that investors are presented with high-quality investment options.`,
+      `Opportunity for liquidity through the Cadre Secondary Market: Cadre offers a secondary market for its investors, providing an opportunity to sell their investment before the end of the investment term. This feature adds an extra level of liquidity and flexibility to the investment process.`,
+    ],
+    pros: [
+      `Potential for attractive returns and cash yields: Farmland can offer potential for attractive returns and cash yields, and FarmTogether provides access to such opportunities to accredited investors.`,
+
+      `FarmTogether does extensive due diligence and invests its own money: FarmTogether does extensive due diligence on farmland properties and invests its own money alongside investors, aligning interests and minimizing conflicts of interest.`,
+
+      `Low correlation with stocks and other asset classes: Investing in farmland can provide diversification benefits as it has low correlation with stocks and other asset classes, potentially helping to reduce portfolio risk.`,
+    ],
+    cons: [
+      `Only available to accredited investors: FarmTogether is only available to accredited investors, which limits the pool of potential investors who can participate in these opportunities.`,
+
+      `Limited liquidity: Investing in farmland through FarmTogether may not provide the same level of liquidity as investing in publicly traded stocks or other assets that can be bought and sold easily.`,
+
+      `Investing in farmland carries risk: Investing in farmland carries risks, including risks related to weather, pests, land prices, and regulatory changes. It is important to carefully evaluate these risks before investing.`,
+    ],
+    minInv: `$15,000`,
+    asstClass: `Farmland`,
+  },
+  {
+    id: "fourth",
+    name: "Royalty exchange",
+    rating: "3.5",
+    link: "https://www.royaltyexchange.com/",
+    linkPara: `Royalties are a common way for musicians and authors to earn money, with some earning millions of dollars, even long after their death. However, smaller artists may not have access to the same opportunities as big names signed by major record labels or investors. This is where Royalty Exchange comes in. It is an online marketplace that connects investors seeking high yields with artists who want upfront money for their royalties.`,
+    paras: [
+      `With Royalty Exchange, artists can sell all or a portion of their royalty streams of income for several years, while investors pay an upfront price for the royalties and receive passive income for a specified period. The platform provides investors and artists with a significant amount of data to help them evaluate investment opportunities, including the option for artists to "NFT" their music.`,
+      `While this type of investment may be attractive to those seeking a high yield investment outside of traditional stocks and bonds, it is important to consider the fees and risks involved. Royalty Exchange charges a 1% investor fee (minimum $500) and a 15% seller fee. Investing in royalties is also expensive and risky, with potential risks including the performance of the artist, changes in the music industry, and legal disputes.`,
+      `In summary, Royalty Exchange offers a unique platform for investors seeking high yields and artists who want upfront money for their royalties. While it may not be suitable for everyone, those who are willing to take on the risks and fees involved may find it a worthwhile investment opportunity.
+      `,
+    ],
+    pros: [
+      `Fluctuating but regular cash flow: Investors who purchase royalties on Royalty Exchange can receive regular passive income for a specified period.`,
+
+      `Very low correlation with other asset classes: Royalties can offer diversification benefits as they have low correlation with traditional asset classes like stocks and bonds.`,
+
+      `Transparent auction process: The platform offers a transparent auction process, which allows investors to evaluate opportunities and decide if they want to participate.`,
+    ],
+    cons: [
+      `Some auctions have high minimum bids ($100K or more): The minimum bids for some auctions on Royalty Exchange can be high, which may limit access for some investors.`,
+
+      `Inherent uncertainty in future cash flow from royalties: Future royalty payments can be uncertain, and investors may not receive the expected cash flow.`,
+
+      `Rights transactions can be incredibly complex: Buying and selling royalty rights can be complex, and investors may need to do extensive due diligence before making an investment decision.`,
+    ],
+    minInv: `$5,000`,
+    asstClass: `Music Royalties`,
+  },
+  {
+    id: "fifth",
+    name: "Forge",
+    rating: "3.0",
+    link: "https://forgeglobal.com/",
+    linkPara: `is an investment platform that enables both individual and institutional investors to invest in privately traded companies and startups. The platform provides a valuable service for large funds that want to diversify into this asset class. Investors can express interest in a particular company, and Forge Global works to locate available shares and facilitate the transaction. Forge Global is primarily focused on institutional investors, but individual investors can also invest on the platform as long as they meet strict requirements, such as being accredited and investing a minimum of $100,000 per company.`,
+    paras: [
+      `On the Forge platform, investors can browse hundreds of different private companies and view financial and fundraising information, as well as a list of key investors. If a company seems appealing, investors can express interest in investing by filling out a form with the number of shares they want to purchase. Once the investment is made, Forge also provides custodial services to securely hold the private company shares. However, since there is no active secondary market for many of these companies, it can potentially take weeks or months for the investment to go through.`,
+      `Overall, Forge Global provides a way for investors to invest in well-known startups and privately traded companies, which may not be available on other platforms. However, the strict requirements and high minimum investment per company make it more suitable for institutional investors than individual investors. Additionally, since these companies are privately traded, there may be limited information available to investors and inherent risks associated with investing in startups.`,
+    ],
+    pros: [
+      `Access to well-known startups: Investors on Forge Global have the opportunity to invest in some of the most well-known and highly valued startups in the world, such as Epic Games, Juul, and Instacart.`,
+
+      `Strong track record: Forge Global has a strong track record of successfully facilitating transactions between investors and startups, and providing custodial services to hold private company shares.`,
+
+      `White glove service: Forge Global offers a personalized and high-touch service to investors, with Private Market Specialists who work with clients to find and invest in companies that align with their investment goals.`,
+    ],
+    cons: [
+      `Very high minimum investment: The minimum investment on Forge Global is $100,000 per company, which is a significant barrier to entry for many individual investors.`,
+
+      `Accredited investors only: In order to invest on Forge Global, investors must be accredited, meaning they must meet certain financial requirements related to income or net worth. This excludes many potential investors who may not meet these requirements.`,
+
+      `5% Commission: Forge Global charges a 5% commission on all transactions, which is higher than many other investment platforms. This can eat into potential returns for investors.`,
+    ],
+    minInv: `$100,000`,
+    asstClass: `Startups`,
+  },
+];
