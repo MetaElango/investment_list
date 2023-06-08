dd5f81500f87d0bae1c3e9ab8f2124094e1cdbcd
Elangodevguy <elangodevguy@gmail.com>
2023-06-08 17:10:18 +0530

    added responsiveness

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,6 +1,3 @@
-// "use client";
-import styles from "./styles.module.css";
-
 export async function generateStaticParams() {
   const res = await fetch(`https://hedonovaagri.com/wp-json/wp/v2/posts`, {
     next: { revalidate: 60 },
@@ -21,6 +18,12 @@ async function getPost(params) {
 
 export default async function Post({ params }) {
   const post = await getPost(params);
+  const date = new Date(post.date.split("T")[0]);
+  const formattedDate = date.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
 
   return (
     <div
@@ -30,14 +33,14 @@ export default async function Post({ params }) {
         justifyContent: "center",
         minHeight: "100vh",
       }}
-      className="blogs"
+      className="blog"
     >
-      <div style={{ width: "50%" }}>
+      <div className="blog-container">
         <h1 style={{ color: "white", fontSize: "2.2rem" }}>
           {post.title.rendered}
         </h1>
         <p style={{ color: "#b1b1b1", fontSize: "1rem" }}>
-          Created at: {post.date}
+          Created at: {formattedDate}
         </p>
         <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />{" "}
       </div>
