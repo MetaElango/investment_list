1fbf2d400b93069e11f93db54cb16b265d8e6c24
Elangodevguy <elangodevguy@gmail.com>
2023-06-14 21:56:25 +0530

    added scroll to top button

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import "./styles/global.css";
 import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/Footer";
 import "./global.css";
+import ScrollButton from "@/components/scrollButton/ScrollButton";
 
 export default function RootLayout({ children }) {
   return (
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
             <Navbar />
             {children}
             <Footer />
+            <ScrollButton />
             <Analytics />
           </ChakraProvider>
         </CacheProvider>
