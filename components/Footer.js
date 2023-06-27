0853bf1d5f227beea6ba1e39915f9d1d7429ee54
Elangodevguy <elangodevguy@gmail.com>
2023-06-27 22:30:44 +0530

    updated email

--- components/Footer.js
+++ components/Footer.js
@@ -73,19 +73,20 @@ export default function Footer() {
             organized and existing under the laws of the State of Delaware. For
             any inquiries or concerns regarding the operation or content of this
             website, you may contact Axba LLC directly at
-            info@bestinvestmentlist.com The information provided on this website
-            is for general informational purposes only. We strive to maintain
-            accuracy but make no representations or warranties about the
-            completeness, accuracy, reliability, suitability, or availability of
-            the website's content. Reliance on such information is at your own
-            risk. The use of third-party names, trademarks, logos, or
-            proprietary designations is purely for informational purposes and
-            does not imply endorsement or affiliation. All trademarks,
-            registered trademarks, and logos belong to their respective owners.
-            We have no control over third-party websites and disclaim liability
-            for any damages incurred. We make no warranties regarding website
-            functionality or uninterrupted access. Seek professional legal
-            advice for your specific circumstances and jurisdiction.
+            info@bestinvestmentlist.com. The information provided on this
+            website is for general informational purposes only. We strive to
+            maintain accuracy but make no representations or warranties about
+            the completeness, accuracy, reliability, suitability, or
+            availability of the website's content. Reliance on such information
+            is at your own risk. The use of third-party names, trademarks,
+            logos, or proprietary designations is purely for informational
+            purposes and does not imply endorsement or affiliation. All
+            trademarks, registered trademarks, and logos belong to their
+            respective owners. We have no control over third-party websites and
+            disclaim liability for any damages incurred. We make no warranties
+            regarding website functionality or uninterrupted access. Seek
+            professional legal advice for your specific circumstances and
+            jurisdiction.
           </Text>
           {/* <Stack direction={"row"} spacing={6}>
             <SocialButton label={"Twitter"} href={"#"}>
